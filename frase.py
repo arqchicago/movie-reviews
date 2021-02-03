608aa62257c7b46fdfd14535f3c67e3da6fe5bb0
Ahmad Qadri <arqchicago@gmail.com>
2021-02-02 23:52:49 -0500

    added initial cleanup of review text

--- frase.py
+++ frase.py
@@ -4,30 +4,26 @@ Created on Wed Jan 13 23:20:38 2021
 
 @author: Ahmad
 """
+from string import punctuation
 
-class word():
+class parola():
 
     def __init__(self, word):
         self.word = word
 
-
     def get_word(self):
         return self.word
-
 
     def get_length(self):
         return len(self.word)
 
-
     def get_char_list(self):
         return list(self.word)
-
 
     def get_char_count(self, char):
         word_list = self.get_char_list()
         n = sum(1 for i in word_list if i==char)
         return n
-
 
     def remove_char(self, char, repchar, inplace=False):
         if inplace:
@@ -37,7 +33,6 @@ class word():
             replaced_word = self.word.replace(char, repchar)
             return replaced_word
 
-
     def get_chars_count_dict(self):
         chars_num_dict = {}
         unique_char_list = list(set(self.word))
@@ -46,7 +41,6 @@ class word():
             chars_num_dict[char] = self.get_char_count(char)
         
         return chars_num_dict
-
 
     def get_chars_pct_dict(self):
         chars_num_dict = {}
@@ -58,9 +52,7 @@ class word():
         return chars_num_dict
 
 
-
-
-class sentence():
+class frase():
 
     def __init__(self, sentence):
         self.sentence = sentence
@@ -71,6 +63,24 @@ class sentence():
     def get_words(self):
         return list(self.sentence.split())
 
+
+class paragrafo():
+
+    def __init__(self, paragraph):
+        self.paragraph = paragraph
+
+    def get_paragraph(self):
+        return self.paragraph
+
+    def remove_punctuation(self):
+        self.paragraph = self.paragraph.translate(str.maketrans('', '', punctuation))
+
+    def remove_extra_spaces(self):
+        self.paragraph = ' '.join(self.paragraph.split())
+
+    def remove_numeric_chars(self):
+        numeric_list = [0,1,2,3,4,5,6,7,8,9]
+        self.paragraph = ' '.join([i for i in self.paragraph.split() if i not in numeric_list])
 
 if __name__ == '__main__':
 
--- main.py
+++ main.py
@@ -5,15 +5,27 @@ Created on Wed Jan 13 23:20:38 2021
 @author: Ahmad
 """
 
-from frase import word, sentence
+from frase import parola, frase, paragrafo
 from os import listdir
 from os.path import isfile, join
+from string import punctuation
+import re
+
 
 if __name__ == '__main__':
+
+    special_char_list = list(punctuation)
     neg_folder = 'data//neg'
     pos_folder = 'data//pos'
 
     neg_files = [f for f in listdir(neg_folder) if isfile(join(neg_folder, f))]
     pos_files = [f for f in listdir(pos_folder) if isfile(join(pos_folder, f))]
     
-    print(pos_files)
+    with open(neg_folder+'//'+neg_files[0], 'r') as file:
+        text = file.read().replace('\n', '')
+
+    review = paragrafo(text)
+    review.remove_punctuation()
+    review.remove_extra_spaces()
+    review.remove_numeric_chars()
+    print(review.get_paragraph())
